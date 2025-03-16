cc735b7c6bac33234bc3aff801a2ed26ca492eb9
Roberta Jesus <roberta@greenlegis.com.br>
2025-03-15 21:27:02 -0300

    Atualizando Cypress para a versão 14.2.0

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,39 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
-// Cypress.Commands.add("buscarClientes", (page = 1, limit = 10, size = "All", industry = "All") => {
-//   cy.request({
-//     method: "GET",
-//     url: `${Cypress.env("apiUrl")}/customers?page=${page}&limit=${limit}&size=${size}&industry=${industry}`,
-//   }).then((response) => {
-//     expect(response.status).to.eq(200);
-//     return response.body;
-//   });
-// });
-
 Cypress.Commands.add('getClientesPorPagina', (pagina) => {
   return cy.request({
     method: 'GET',
